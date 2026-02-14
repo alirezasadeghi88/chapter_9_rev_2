bdf62a5446587c46ab0e20010f0502bdd2946666
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-14 08:17:29 -0800

    add TV

---
+++ src/homework1/TestTV.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class TestTV {
+    public static void main(String[] args) {
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
+
+        System.out.println("tv1's channel is " + tv1.channel +
+                " and volume level is " + tv1.volumeLevel);
+
+    }
+}
